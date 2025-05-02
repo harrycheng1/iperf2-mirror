7cca39c3a1f1f28f083bcc7a4db22fd90e4d0794
Robert McMahon <rjmcmahon@rjmcmahon.com>
2025-05-02 15:18:42 -0700

    ce duration on client

--- src/Locale.c
+++ src/Locale.c
@@ -577,10 +577,10 @@ const char report_client_bb_bw_triptime_format[] =
 "%s" IPERFTimeFrmt " sec  OWD (ms) Cnt=%" PRIdMAX " TX=%.3f/%.3f/%.3f/%.3f RX=%.3f/%.3f/%.3f/%.3f Asymmetry=%.3f/%.3f/%.3f/%.3f%s\n";
 
 const char report_bw_pps_enhanced_header[] =
-"[ ID] Interval" IPERFTimeSpace "Transfer     Bandwidth   Write/Err/Timeo  PPS  CE=cnt(%) duration avg/min/max ms\n";
+"[ ID] Interval" IPERFTimeSpace "Transfer     Bandwidth   Write/Err/Timeo  PPS  CE=cnt(%) duration avg/min/max ms act-cnt\n";
 
 const char report_bw_pps_enhanced_format[] =
-"%s" IPERFTimeFrmt " sec  %ss  %ss/sec   %" PRIdMAX "/%" PRIdMAX "/%" PRIdMAX "%8.0f pps %" PRIdMAX "(%.1f%%) %.3f/%.3f/%.3f%s\n";
+"%s" IPERFTimeFrmt " sec  %ss  %ss/sec   %" PRIdMAX "/%" PRIdMAX "/%" PRIdMAX "%8.0f pps %" PRIdMAX "(%.1f%%) %.3f/%.3f/%.3f %" PRIdMAX "%s\n";
 
 const char report_sumcnt_bw_pps_enhanced_header[] =
 "[SUM-cnt] Interval" IPERFTimeSpace "Transfer     Bandwidth      Write/Err  PPS\n";
--- src/ReportOutputs.c
+++ src/ReportOutputs.c
@@ -1235,6 +1235,7 @@ void udp_output_write_enhanced (struct TransferInfo *stats) {
 	   (stats->final ? stats->CE_Duration.total.mean : stats->CE_Duration.current.mean),
 	   (stats->final ? stats->CE_Duration.total.min : stats->CE_Duration.current.min),
 	   (stats->final ? stats->CE_Duration.total.max : stats->CE_Duration.current.max),
+	   (stats->final ? stats->CE_Duration.total.cnt : stats->CE_Duration.current.cnt),
 	   (stats->common->Omit ? report_omitted : ""));
     cond_flush(stats);
 }
